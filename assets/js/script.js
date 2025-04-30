a7a5208826bfcade783de4765773700ade1db15d
Ahmed Mohamed Abdulaziz <ahmedmohamedabdulaziz2023@gmail.com>
2025-04-30 11:03:00 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,17 @@ menuIcon.onclick=()=> {
      menuIcon.classList.toggle('bx-x')
      navbar.classList.toggle('active');
 }
+document.addEventListener("DOMContentLoaded", function () {
+     const sr = ScrollReveal({
+       origin: 'top',
+       distance: '60px',
+       duration: 2000,
+       delay: 200,
+     });
+   
+     sr.reveal('.home-content, .about-img, .heading, .about-content');
+     sr.reveal('.home-img', { delay: 400 });
+     sr.reveal('.social-icons a', { interval: 200 });
+     sr.reveal('.service-box, .project-card, .input-box, .input-group-2', { interval: 200 });
+   });
+   
